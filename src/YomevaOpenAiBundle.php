10d750c6cac8fa9880dad5928cff6eb0d436056e
Arthur FALQUE PIERROTIN <arthur.fp.dev@pm.me>
2024-10-02 16:55:17 +0200

    Trying to create an alias for the service

--- src/YomevaOpenAiBundle.php
+++ src/YomevaOpenAiBundle.php
@@ -2,12 +2,22 @@
 
 namespace Yomeva\OpenAiBundle;
 
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class YomevaOpenAiBundle extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+            ->scalarNode('open_ai_api_key')->end()
+            ->end() // children
+        ;
+    }
+
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('../config/services.yaml');
