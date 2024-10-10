bbf840e54349eabe2d6f596801e4ace26ad5a164
Arthur FALQUE PIERROTIN <arthur.fp.dev@pm.me>
2024-10-10 15:42:34 +0200

    Validation for CodeInterpreterResources

--- src/Model/Tool/CodeInterpreterResources.php
+++ src/Model/Tool/CodeInterpreterResources.php
@@ -2,9 +2,16 @@
 
 namespace Yomeva\OpenAiBundle\Model\Tool;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class CodeInterpreterResources
 {
-    public function __construct(public array $fileIds = [])
+    public function __construct(
+
+        #[Assert\Count(20)]
+        #[Assert\All([new Assert\Type('string')])]
+        public array $fileIds = []
+    )
     {
     }
 }
