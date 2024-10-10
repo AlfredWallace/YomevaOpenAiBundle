d2e6e96a8ac5520894f5241c23ad159ab04b2700
Arthur FALQUE PIERROTIN <arthur.fp.dev@pm.me>
2024-10-10 16:10:40 +0200

    Adding validation for the $vectorStoreIds of the FileSearchResources

--- src/Model/Tool/FileSearchResources.php
+++ src/Model/Tool/FileSearchResources.php
@@ -2,10 +2,17 @@
 
 namespace Yomeva\OpenAiBundle\Model\Tool;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class FileSearchResources
 {
     public function __construct(
+
+        #[Assert\Count(max: 1)]
+        #[Assert\All([new Assert\Type('string')])]
         public array $vectorStoreIds = [],
+
+
         public array $vectorStores = []
     )
     {
