ecf7fa438a1f202d557a99065b53c35a0ccc348f
Arthur FALQUE PIERROTIN <arthur.fp.dev@pm.me>
2024-10-11 16:19:30 +0200

    Adding a constraint on ToolResources to prevent from sending empty object/jsonArray to the OpenAI API

--- src/Model/Tool/ToolResources.php
+++ src/Model/Tool/ToolResources.php
@@ -7,6 +7,10 @@ use Yomeva\OpenAiBundle\Model\Tool\CodeInterpreter\CodeInterpreterResources;
 use Yomeva\OpenAiBundle\Model\Tool\FileSearch\FileSearchResources;
 
 #[Assert\Cascade]
+#[Assert\Expression(
+    "this.codeInterpreter === null and this.fileSearch === null",
+    message: "You must provide at least one of the two ToolResources."
+)]
 class ToolResources
 {
     public function __construct(
