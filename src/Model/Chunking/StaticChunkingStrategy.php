9fb7ba4624e44fdac5e2ca401e605f83189488d6
Arthur FALQUE PIERROTIN <arthur.fp.dev@pm.me>
2024-10-11 14:09:50 +0200

    Fix StaticChunkingStrategy model property name to match OpenAI API

--- src/Model/Chunking/StaticChunkingStrategy.php
+++ src/Model/Chunking/StaticChunkingStrategy.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[Assert\Cascade]
 class StaticChunkingStrategy extends ChunkingStrategy
 {
-    public function __construct(public StaticChunkingStrategyStatic $staticChunkingStrategyStatic)
+    public function __construct(public StaticChunkingStrategyStatic $static)
     {
         parent::__construct('static');
     }
