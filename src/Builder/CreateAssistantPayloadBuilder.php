505b72b7ff4dc01fa6f3a183557814e5d8b7a2e7
Arthur FALQUE PIERROTIN <arthur.fp.dev@pm.me>
2024-10-14 11:42:48 +0200

    Adding methods in the builder for all direct children

--- src/Builder/CreateAssistantPayloadBuilder.php
+++ src/Builder/CreateAssistantPayloadBuilder.php
@@ -3,6 +3,9 @@
 namespace Yomeva\OpenAiBundle\Builder;
 
 use Yomeva\OpenAiBundle\Model\Assistant\CreateAssistantPayload;
+use Yomeva\OpenAiBundle\Model\ResponseFormat\ResponseFormat;
+use Yomeva\OpenAiBundle\Model\Tool\Tool;
+use Yomeva\OpenAiBundle\Model\Tool\ToolResources;
 
 class CreateAssistantPayloadBuilder implements PayloadBuilderInterface
 {
@@ -18,15 +21,68 @@ class CreateAssistantPayloadBuilder implements PayloadBuilderInterface
         return $this->createAssistantPayload;
     }
 
-    public function addName(string $name): self
+    public function setName(string $name): self
     {
         $this->createAssistantPayload->name = $name;
         return $this;
     }
 
-    public function addDescription(string $description): self
+    public function setDescription(string $description): self
     {
         $this->createAssistantPayload->description = $description;
+        return $this;
+    }
+
+    public function setInstructions(string $instructions): self
+    {
+        $this->createAssistantPayload->instructions = $instructions;
+        return $this;
+    }
+
+    public function addTool(Tool $tool): self
+    {
+        $this->createAssistantPayload->tools[] = $tool;
+        return $this;
+    }
+
+    /**
+     * @param Tool[] $tools
+     */
+    public function setTools(array $tools): self
+    {
+        foreach ($tools as $tool) {
+            $this->addTool($tool);
+        }
+        return $this;
+    }
+
+    public function setToolResources(ToolResources $toolResources): self
+    {
+        $this->createAssistantPayload->toolResources = $toolResources;
+        return $this;
+    }
+
+    public function setMetadata(array $metadata): self
+    {
+        $this->createAssistantPayload->metadata = $metadata;
+        return $this;
+    }
+
+    public function setTemperature(float $temperature): self
+    {
+        $this->createAssistantPayload->temperature = $temperature;
+        return $this;
+    }
+
+    public function setTopP(float $topP): self
+    {
+        $this->createAssistantPayload->topP = $topP;
+        return $this;
+    }
+
+    public function setResponseFormat(string|ResponseFormat $responseFormat): self
+    {
+        $this->createAssistantPayload->responseFormat = $responseFormat;
         return $this;
     }
 }
