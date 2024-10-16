d5464d82a510d0452b25eb66cf116e9667360320
Arthur FALQUE PIERROTIN <arthur.fp.dev@pm.me>
2024-10-16 11:47:53 +0200

    Initializing the tests with basic config and dummy first test

---
+++ tests/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Yomeva\OpenAiBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class DummyTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
