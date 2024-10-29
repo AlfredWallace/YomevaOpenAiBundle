b7d3cdadb686d1f122e5f8652c7d731f7c0b1c86
Arthur FALQUE PIERROTIN <arthur.fp.dev@pm.me>
2024-10-29 14:42:45 +0100

    Small security improvement to check if we execute tests NOT in prod

--- tests/integration/ClientTestCase.php
+++ tests/integration/ClientTestCase.php
@@ -7,13 +7,17 @@ use Yomeva\OpenAiBundle\Service\OpenAiClient;
 
 abstract class ClientTestCase extends TestCase
 {
-    // todo garde fou pas la prod
+    // todo sécurité pour ne pas accidentellement taper sur la prod : vérifier via l'API OpenAI les Projects
+    // todo et/ou les service-accounts et/ou les users pour savoir si on tape bien sur 'RelaSync staging'
     protected static OpenAiClient $client;
 
     public static function setUpBeforeClass(): void
     {
         // this has to be a key for an OpenAI project used for tests where all data can be deleted
-        self::$client = new OpenAiClient($_ENV['OPEN_AI_API_KEY']);
+        // having a different name than the one used in the project where you install the bundle
+        // (OPEN_AI_API_TEST_KEY instead of OPEN_AI_API_KEY) is a small security enhancement to
+        // reduce the chances to run tests in a prod environment
+        self::$client = new OpenAiClient($_ENV['OPEN_AI_API_TEST_KEY']);
         parent::setUpBeforeClass();
     }
 }
