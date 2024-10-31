faf1e3ddabe62012bfd7afb8eb931cb10d0ec5af
Arthur FALQUE PIERROTIN <arthur.fp.dev@pm.me>
2024-10-31 11:33:12 +0100

    Adding setters for the CreateRun builder

--- src/Builder/Payload/Run/CreateRunPayloadBuilder.php
+++ src/Builder/Payload/Run/CreateRunPayloadBuilder.php
@@ -2,6 +2,7 @@
 
 namespace Yomeva\OpenAiBundle\Builder\Payload\Run;
 
+use Yomeva\OpenAiBundle\Model\Message\CreateMessagePayload;
 use Yomeva\OpenAiBundle\Model\Run\CreateRunPayload;
 
 class CreateRunPayloadBuilder implements CreateRunBasePayloadBuilderInterface
@@ -18,5 +19,36 @@ class CreateRunPayloadBuilder implements CreateRunBasePayloadBuilderInterface
     public function getPayload(): CreateRunPayload
     {
         return $this->createRunPayload;
+    }
+
+    public function setAdditionalInstructions(string $additionalInstructions): self
+    {
+        $this->createRunPayload->additionalInstructions = $additionalInstructions;
+        return $this;
+    }
+
+    /**
+     * To easily add a message, use the Yomeva\OpenAiBundle\Builder\Payload\Message\CreateMessagePayloadBuilder
+     * Example :
+     * $openAiClient->addAdditionalMessage(
+     *     (new CreateMessagePayloadBuilder(Role::User, "message part 1"))
+     *          ->addText("message part 2")
+     *          ...
+     *          ->getPayload()
+     * );
+     */
+    public function addAdditionalMessage(CreateMessagePayload $message): self
+    {
+        $this->createRunPayload->additionalMessages[] = $message;
+        return $this;
+    }
+
+    /**
+     * See addAdditionalMessage doc
+     */
+    public function setAdditionalMessages(array $additionalMessages): self
+    {
+        $this->createRunPayload->additionalMessages = $additionalMessages;
+        return $this;
     }
 }
